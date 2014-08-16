97b09af0668291ef13046c196c93b3901fce4e80
rennyatwork <rennyatwork@gmail.com>
2014-08-16 13:06:20 -0400

    test commit

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,34 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+areEqual <-function (pMatrix1, pMatrix2)
+{
+  is.matrix(pMatrix1) && is.matrix(pMatrix2) && dim(pMatrix1) == dim(pMatrix2) && all(pMatrix1 == pMatrix2)
+}
 
+makeCacheMatrix <- function(pMatrix = matrix()) {
+
+  
+  #cmMatrix <- pMatrix
+  cmInverse <- NULL
+  
+  get <- function() pMatrix
+  
+  set <- function (pMat)
+  {
+    #cmMatrix <<-pMatrix
+    pMatrix <<-pMat
+    cmInverse <<-NULL
+  }
+  
+  getInv <- function() cmInverse
+  
+  setInv <-function()
+  {
+    cmInverse <<-solve(cmMatrix)
+  }
+  
+  list(get=get, set=set, getInv=getInv, setInv = setInv)
 }
 
 
